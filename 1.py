2de65b13d8a50c42a5c0f993781645a2bde860a7
DheepthaRai <51468745+DheepthaRai@users.noreply.github.com>
2020-11-12 21:50:15 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,11 +1,29 @@
+#I have made some sort of a logical error in this and am currently trying to work it out. So it's under progress... Feel free to check it out and may be find the key part I'm missing out on, if you want to :)
+
 z = int(input("Enter a number: "))
 
-if z>=0:
-    if z%2==0:                                    #checks if it's an even number
-        if ((z//2)%2)==0:                         #checks for required condition
-            print("True :)")
+def iter(z):
+    c = True
+    i = 0
+    while (z%2)!=0:
+        z=z//2
+        if z==int(z) and z%2==0:
+            i=i+1
+            iter(z)
+        elif z!=int(z) or z%2!=0:
+            c = False
+        elif z/2==1:
+            c = True
         else:
-            print("False :/")
+            c = False
+    if c == True and i%2==0:
+        return True
+    else:
+        return False
+
+if z>=0:
+    if z%2==0:
+        print(iter(z))
     else:
         print("False")
 else:

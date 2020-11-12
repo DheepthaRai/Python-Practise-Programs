c1ad2f48b8226efe31eafb134a9a29b4fd7921c7
DheepthaRai <51468745+DheepthaRai@users.noreply.github.com>
2020-11-12 21:09:50 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,8 +1,8 @@
 z = int(input("Enter a number: "))
 
 if z>=0:
-    if z%2==0:
-        if ((z//2)%2)==0:
+    if z%2==0:                                    #checks if it's an even number
+        if ((z//2)%2)==0:                         #checks for required condition
             print("True :)")
         else:
             print("False :/")

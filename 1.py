6df96bb31252d40444151704abf13908f8d404f5
DheepthaRai <51468745+DheepthaRai@users.noreply.github.com>
2020-11-13 16:55:59 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,29 +1,17 @@
-#I have made some sort of a logical error in this and am currently trying to work it out. So it's under progress... But feel free to check it out and may be find the key part
-#I'm missing out on, if you want to :)
-
 z = int(input("Enter a number: "))
 
-def iter(z):
-    c = True
-    i = 0
-    while (z%2)!=0:
-        z=z//2
-        if z==int(z) and z%2==0:
-            i=i+1
-            iter(z)
-        elif z!=int(z) or z%2!=0:
-            c = False
-        elif z/2==1:
-            c = True
-        else:
-            c = False
-    if c == True and i%2==0:
-        return True
-    else:
+def iter(n):
+    if (n == 0):
         return False
+    while (n != 1):
+        if (n%2 != 0):
+            return False
+        n = n // 2
+
+    return True
 
 if z>=0:
-    if z%2==0:
+    if iter(z):
         print(iter(z))
     else:
         print("False")

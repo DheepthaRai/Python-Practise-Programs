495a722785acf7aedef609eca4abb1d6aa8da7bc
DheepthaRai <51468745+DheepthaRai@users.noreply.github.com>
2020-11-13 17:37:17 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,20 @@
+import math as m
+
+def Log(x):
+    return (m.log10(x) / m.log10(m.sqrt(x)))
+
+def isSquare(n):
+    return (m.ceil(Log(n)) == m.floor(Log(n)) == 2)
+
+z = int(input("Enter number: "))
+
+if z == int(z) and z>0:
+    if isSquare(z):
+        if m.sqrt(z) == int(m.sqrt(z)):
+            print("True. The given number %s is the square of %s" %((z),int((m.sqrt(z)))))
+        else:
+            print("False :/")
+    else:
+        print("False")
+else:
+    print("Not a positive integer")

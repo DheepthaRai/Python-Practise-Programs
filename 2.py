053e84ba231f11f7b0631cf9ae7779a2e11e5a4c
DheepthaRai <51468745+DheepthaRai@users.noreply.github.com>
2020-11-13 17:01:09 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,18 @@
+z = int(input("Enter a number: "))
+
+def iter(n):
+    if (n == 0):
+        return False
+    while (n != 1):
+        if (n%3 != 0):
+            return False
+        n = n // 3
+    return True
+
+if z>=0:
+    if iter(z):
+        print(iter(z))
+    else:
+        print("False")
+else:
+    print("Not a positive integer")
